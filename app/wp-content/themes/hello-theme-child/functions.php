4c400b769ad074e57835e962cf519dba04454586
Clecyo Freitas <clecyo.freitas@adventistas.org>
2024-04-18 10:39:17 -0300

    Adiciona integração aprimorada de dados de embed na API REST

--- app/wp-content/themes/hello-theme-child/functions.php
+++ app/wp-content/themes/hello-theme-child/functions.php
@@ -10,38 +10,39 @@
  * @package HelloElementorChild
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
+define('HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0');
 
 /**
  * Load child theme scripts & styles.
  *
  * @return void
  */
-function hello_elementor_child_scripts_styles() {
+function hello_elementor_child_scripts_styles()
+{
 
-	wp_enqueue_style(
-		'hello-elementor-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		[
-			'hello-elementor-theme-style',
-		],
-		HELLO_ELEMENTOR_CHILD_VERSION
-	);
-
+    wp_enqueue_style(
+        'hello-elementor-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        [
+            'hello-elementor-theme-style',
+        ],
+        HELLO_ELEMENTOR_CHILD_VERSION
+    );
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+add_action('wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20);
 
-function update_SiteMeta($post_id) {
+function update_SiteMeta($post_id)
+{
 
     if (get_post_type($post_id) == 'timeline') {
 
         $values = get_fields($post_id);
         if (isset($values['ct']) && is_array($values['ct'])) {
-            
+
             foreach ($values['ct'] as $layout) {
                 if (isset($layout['acf_fc_layout']) && $layout['acf_fc_layout'] === 'ct-site') {
                     $url = isset($layout['ct-site-url']) ? $layout['ct-site-url'] : '';
@@ -80,11 +81,30 @@ function update_SiteMeta($post_id) {
                         update_field('ct_0_ct-site-description', $description, $post_id);
                         update_field('ct_0_ct-site-image_url', $imageUrl, $post_id);
                     }
-                    break; 
+                    break;
                 }
             }
         }
     }
 }
 
- add_action( 'acf/save_post', 'update_SiteMeta' );
+
+add_action('acf/save_post', 'update_SiteMeta');
+
+add_action('rest_api_init', function () {
+    register_rest_field(
+        'timeline',
+        'content',
+        array(
+            'get_callback'    => 'api_rest_customization',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+});
+
+function api_rest_customization($post)
+{
+
+    return is_array($content = get_field('ct', $post['id'])) ? $content[0] : null;
+}
